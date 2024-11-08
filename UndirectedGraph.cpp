c31cdf9751cf4e8f6a33746ae0df685c0e19c2b3
vablov <uyenv.ngn@gmail.com>
2024-11-08 21:49:16 +0700

    Finished problem b) and d)

--- UndirectedGraph.cpp
+++ UndirectedGraph.cpp
@@ -11,22 +11,23 @@ struct Graph{
 
 /*
 Viet chuong trinh doc ma tran ke cua do thi. Xac dinh va in ra:
-	a. TODO: So canh (edges), so dinh (vertices) cua do thi?
+	a. done: So canh (edges), so dinh (vertices) cua do thi?
 	
 		DESAU: Phan biet 2 TH do thi co huong va do thi vo huong
 		
-	b. TODO: Xuat bac cua tat ca cac dinh
+	b. done: Xuat bac cua tat ca cac dinh
 		Bac cua 1 dinh trong do thi vo huong la
 			tong cac canh ke voi dinh do.
 		Khuyen duoc tinh 2 lan.
 		
 	c. TODO: Cac dinh co so bac max/ min,
 		dinh bac chan, dinh bac le
-	d. TODO: Cac dinh co lap, dinh treo
+	d. done: Cac dinh co lap, dinh treo
 		Dinh co lap: bac = 0
 		Dinh treo: bac = 1
 */
 
+//read txt file from path
 void readGraph(Graph& g){
 	ifstream inFile;
 	
@@ -62,10 +63,39 @@ void displayNumVnE(Graph g){
 	cout << edgeNum << "\n";
 }
 
+
+//display the degree of each node (each row)
 void displayDeg(Graph g){
-	//TODO
+	/*In a nut shell:
+	Check from [i] = 0
+		bool isolated = false;
+		int connection = 0;
+		from [j]
+		if adj[i][j] == 0 && adj[j][i] == 0
+			isolated = true;
+		if adj[i][j] != 0 || adj[j][i] != 0
+			connection += 1;
+		if (isolated) cout << "Dinh co lap: " << i << "\n";
+		if (connection != 1)
+			cout << "Dinh " << i << " co bac: " << connection << endl;
+		else cout << "Dinh treo: " << i << "\n";
+	*/
+	for (int i= 0; i < g.ver; i++){
+		bool isolated = false;
+		int connection = 0;
+		for (int j= 0; j < g.ver; j++){
+			if (g.adj[i][j] == 0)  isolated = true;
+			else connection += 1;
+		}
+		cout << "Dinh " << i;
+	 	if (isolated == 1 && connection == 0) cout << " la dinh co lap" << "\n";
+		else if (connection == 1) cout << " la dinh treo" << "\n";
+		else if (connection != 1) cout << " co so bac la: " << connection << endl;
+	}
+
 }
 
+//check if the given graph was typed as undirected
 bool undirectedGraph(Graph g){
 	for (int i= 0; i < g.ver; i++){
 		for (int j= 0; j < g.ver; j++){
@@ -74,33 +104,15 @@ bool undirectedGraph(Graph g){
 	}
 	return 1;
 }
-/*
-void displaySpecialVertices(Graph g){
-	for (int i= 0; i < g.ver; i++){
-			for (int j= 0; j < g.ver; j++){
-				if ( (g.adj[i][j] == 0) && (g.adj[j][i]==0) ){
-					cout << "Dinh co lap:\n"
-					cout << g.adj[i] << " ";
-				}
-				cout << "\n"
-				if ( (g.adj[i][j] == 1) && (g.adj[j][i]==1) ){
-					cout << "Dinh treo:\n"
-					cout << g.adj[i] << " ";
-				}
-				cout << "\n";
-			}
-		}
-	}
-}
-*/
+
 int main(){
 	Graph g;
 	
 	readGraph(g);
 
-	displayNumVnE(g);
+	//displayNumVnE(g);
 
-	
+	displayDeg(g);
 	
 	return 0;
 }
